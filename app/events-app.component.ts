5b7a394889c6363ca785667376326d6fd6b50c67
Pedro Rodriguez <parmando88@gmail.com>
2017-01-31 13:37:28 -0500

    Initial commit

---
+++ app/events-app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'events-app',
+    template: '<events-list></events-list>'
+    
+})
+
+export class EventsAppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
